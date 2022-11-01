e1da1266745cec49b2e0c460f9ed1537b2172bff
Danny <dannyhyunsoowilliams@gmail.com>
2022-11-01 22:52:07 +0000

    feat: use storybook webpack5 builder

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -2,4 +2,7 @@ module.exports = {
   stories: ['../components/**/*.stories.?(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-react-native-web'],
   framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
 }
